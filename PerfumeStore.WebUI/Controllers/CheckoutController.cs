16cdb3481ee72fe86a5c1ca09f8661a85a5bc030
Ehmed Mustafa <prince.1987@mail.ru>
2025-08-27 23:37:29 +0400

    continionight

---
+++ PerfumeStore.WebUI/Controllers/CheckoutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PerfumeStore.Application.Services.CartServices;
+
+namespace PerfumeStore.WebUI.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CheckoutController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = 1;
+            var values = await _cartService.GetCartByUserIdWithItemsAsync(userId);
+            return View(values);
+        }
+    }
+}
